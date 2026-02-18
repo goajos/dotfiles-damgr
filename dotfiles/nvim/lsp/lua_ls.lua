c83deea0fbc07833a3f05befc7239bc6335251d8
goajos <jappecremers@gmail.com>
2026-02-18 15:13:15 +0100

    better lua lsp

--- dotfiles/nvim/lsp/lua_ls.lua
+++ dotfiles/nvim/lsp/lua_ls.lua
@@ -4,6 +4,12 @@ return {
   root_markers = { ".git" },
   settings = {
     Lua = {
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      codelens = { enable = true },
+      hint = { enable = true, semicolon = 'Disable' },
       runtime = { version = "LuaJIT" },
       diagnostics = { globals = { "vim" } },
     },
